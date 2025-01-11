c3fd5804715beec3c345c256c64daee0ff26b7e9
Shantanu Kumar <54547460+Shaansh2601@users.noreply.github.com>
2025-01-11 02:02:37 +0000

    Update payment.js

--- payment.js
+++ payment.js
@@ -31,21 +31,6 @@ document.addEventListener('DOMContentLoaded',async() =>{
             messages.innerText = error.message; 
         }
     })
-    const cost = GetURLParameter("cost")
-    console.log(cost);
-    document.getElementById("cost").innerHTML = cost;
+   
 });
-function GetURLParameter(sParam)
-{
-    var sPageURL = window.location.search.substring(1);
-    var sURLVariables = sPageURL.split('&');
-    for (var i = 0; i < sURLVariables.length; i++) 
-    {
-        var sParameterName = sURLVariables[i].split('=');
-        if (sParameterName[0] == sParam) 
-        {
-            return sParameterName[1];
-        }
-    }
-    return null;
-}​
+​
